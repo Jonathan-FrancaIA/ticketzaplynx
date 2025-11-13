f0a7b30fc4b89ebfb213da876c3d478eaa13757e
Jonathan Franca <patriciatrindade809@gmail.com>
2025-11-13 01:55:43 -0300

    removendo arquivos indesejados

--- src/config/debug.ts
+++
@@ -1,1 +0,0 @@
-export const ENABLE_LID_DEBUG = true;
--- src/services/SettingServices/getSettingsValue.ts
+++
@@ -1,8 +0,0 @@
-import Setting from "../../models/Setting";
-
-export const getSettingsValue = async (key: string): Promise<string | undefined> => {
-  const setting = await Setting.findOne({
-    where: { key },
-  });
-  return setting?.value;
-};
